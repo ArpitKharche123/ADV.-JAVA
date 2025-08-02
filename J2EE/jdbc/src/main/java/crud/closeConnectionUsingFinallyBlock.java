5dc0a7cb7ebd4a9e52be0e45a4937c785261ed08
Arpit Kharche <129381820+ArpitKharche123@users.noreply.github.com>
2025-08-02 18:32:36 +0530

    Add files via upload

---
+++ J2EE/jdbc/src/main/java/crud/closeConnectionUsingFinallyBlock.java
@@ -0,0 +1,50 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class closeConnectionUsingFinallyBlock {
+public static void main(String[] args) {
+
+	Connection connection=null;
+	Statement statement=null;
+	
+	String url="jdbc:postgresql://localhost/demo";
+	String user="postgres";
+	String	password="root1234";
+
+	try 
+	{
+	  connection = DriverManager.getConnection(url, user, password);
+		System.out.println("Connnection is established");
+		
+	statement = connection.createStatement();
+		 
+		String query="INSERT INTO employees VALUES(6,'Brijesh','male')";
+		
+		int rows = statement.executeUpdate(query);
+		
+		System.out.println("Data is inserted successfully!!!");
+		System.out.println("No. of rows affected: "+rows);
+	
+	}  catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}finally {
+			try {
+				if(connection!=null) {
+				connection.close();
+				}
+				if(statement!=null) {
+					statement.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+}
+}
---
+++ J2EE/jdbc/src/main/java/crud/update.java
@@ -0,0 +1,52 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class update {
+
+	public static void main(String[] args) {
+		
+		String url="jdbc:postgresql://localhost:5432/demo";
+		String user="postgres";
+		String	password="root1234";
+		
+		try {
+			
+		Connection connection = DriverManager.getConnection(url, user, password);
+		
+		System.out.println("Connnection is established");
+		
+		//Step 3:Create or Prepare the Statement
+		
+		Statement statement = connection.createStatement();
+		
+		//Step 4:Execute the query
+		String query="UPDATE employees "
+				+ "SET name='Emily',gender='female' "
+				+ "WHERE ID=1";
+		
+		//statement.execute(query);
+		
+		//or
+		
+		statement.executeUpdate(query);
+		System.out.println("Data is updated successfully!!!");
+		
+		//Step 5: Close the connection
+		connection.close();
+		statement.close();
+		
+		//Step 6:De-register the Driver if Registered
+		//DriverManager.deregisterDriver(driver);
+		
+	}  catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	}
+
+}
